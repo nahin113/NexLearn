c5bdf06a7972cae2474752c93bd7c19cc90dc831
nahin113 <nahinahmed113@gmail.com>
2026-05-03 00:36:52 +0600

    chore: initialize Next.js app with Tailwind CSS, DaisyUI and project folder structure

---
+++ src/app/profile/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserProfile = () => {
+    return (
+        <div>
+            <h2>This is User Profile</h2>
+        </div>
+    );
+};
+
+export default UserProfile;
